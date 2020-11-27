09711649ca6ed5575efe6fc9ce1192913931086e
AJAY DEBATA <ajay.debata@jda.com>
2020-11-28 01:52:46 +0530

    committed for showing productlist

--- src/App.js
+++ src/App.js
@@ -1,18 +1,74 @@
-function App() {
-  return (
-    <div className="grid-container">
-     <header>
-       <a href="/">Odisha_Hastashilpi</a>
-     </header>
-     <main>
-       Product list
-       Ajay is here
-     </main>
-     <footer>
-       All copy right are Reserved
-     </footer>
+import React from "react";
+import Products from "./components/Products";
+import data from "./data";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      availableCol: "",
+      sort: ""
+    }
+  }
+  render() {
+    return (
+      <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="/">
+            ODISHA_HASTASHILPI
+          </a>
+        </div>
+        <div>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
+        </div>
+      </header>
+      <main>
+        <div>
+          <div className="row center">
+            {data.products.map((product) => (
+              <div key={product._id} className="card">
+                <a href={`/product/${product._id}`}>
+                  <img
+                    className="medium"
+                    src={product.image}
+                    alt={product.name}
+                  />
+                </a>
+                <div className="card-body">
+                  <a href={`/product/${product._id}`}>
+                    <h2>{product.name}</h2>
+                  </a>
+                  <div className="rating">
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                  </div>
+                  <div className="price">Rs- {product.price}</div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </main>
+      <footer className="row center">All right reserved</footer>
     </div>
-  );
+    );
+  }
+
 }
 
 export default App;
